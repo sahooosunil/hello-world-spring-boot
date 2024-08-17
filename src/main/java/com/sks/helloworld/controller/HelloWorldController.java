5342251a1852c4a2ee9ef60959755d8249e3dbb0
“sunil” <sunilsahu0123@gmail.com>
2024-08-17 22:53:27 +0530

    hgf

--- src/main/java/com/sks/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/sks/helloworld/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World!!!!!!!!!! updated4";
     }
 }
